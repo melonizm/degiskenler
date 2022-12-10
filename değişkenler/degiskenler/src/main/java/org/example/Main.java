4038d163cf17bcfad2ba926d99ee2425a53af17e
melo <120278255+melonizm@users.noreply.github.com>
2022-12-10 23:16:40 +0300

    degiskenler
    cok basit olduğu için int ve string dışında double vs fln yazmadım videodakini yaptım yeterli bence

---
+++ değişkenler/degiskenler/src/main/java/org/example/Main.java
@@ -0,0 +1,12 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        int ogrenciSayisi = 10;
+        String mesaj = "öğrenci sayısı : ";
+        System.out.println(mesaj+ogrenciSayisi);
+        System.out.println(mesaj+ogrenciSayisi);
+        System.out.println(mesaj+ogrenciSayisi);
+
+    }
+}
